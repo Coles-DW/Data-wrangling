fb979c818cf90d39e4c91d0d27cc24b295701639
Donovin Coles <148016148+Coles-DW@users.noreply.github.com>
2025-11-06 07:24:41 -0700

    Update scatterplot.py

--- scatterplot.py
+++ scatterplot.py
@@ -1,5 +1,14 @@
+import matplotlib.pyplot as plt; import importlib; importlib.reload(plt)
 # Import numpy as np
 import numpy as np
+
+dict = {
+    'Asia':'red',
+    'Europe':'green',
+    'Africa':'blue',
+    'Americas':'yellow',
+    'Oceania':'black'
+}
 
 # Store pop as a numpy array: np_pop
 np_pop = np.array(pop)
@@ -18,4 +27,27 @@ plt.title('World Development in 2007')
 plt.xticks([1000, 10000, 100000],['1k', '10k', '100k'])
 
 # Display the plot
+plt.show()
+
+# Specify c and alpha inside plt.scatter()
+plt.scatter(x = gdp_cap, y = life_exp, s = np.array(pop) * 2, c = col, alpha = 0.8)
+
+# Previous customizations
+plt.xscale('log') 
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+plt.title('World Development in 2007')
+plt.xticks([1000,10000,100000], ['1k','10k','100k'])
+
+# Show the plot
+plt.show()
+
+# Additional customizations
+plt.text(1550, 71, 'India')
+plt.text(5700, 80, 'China')
+
+# Add grid() call
+plt.grid(True)
+
+# Show the plot
 plt.show()
